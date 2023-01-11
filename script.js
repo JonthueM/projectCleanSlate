4971564d291f104dbc97febc8fcdd89824bb12a9
JonthueM <connect@jonthuemichel.com>
2023-01-11 15:55:14 -0500

    Source Files

---
+++ script.js
@@ -0,0 +1,7 @@
+//Bootstrap Active Links 
+//Links in nav bar becomes active when user is in the section pertaing to the nav item
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+  });
+
+  
